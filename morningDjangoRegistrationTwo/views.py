a1d6b70eaf9e97cef80d32d5b93327a22c3cc10b
khisia <khisam280@gmail.com>
2023-03-06 07:54:00 +0300

    added login feature

--- morningDjangoRegistrationTwo/views.py
+++ morningDjangoRegistrationTwo/views.py
@@ -25,6 +25,54 @@ def add_product(request):
         p_price = request.POST.get("bei")
         product = Product(prod_name=p_name, prod_quantity=p_quantity, prod_price=p_price)
         product.save()
-        messages.success(request, 'Product saved successfully')
+        messages.success(request, 'Product added successfully')
         return redirect('add-product')
     return render(request, 'add-product.html')
+@login_required
+def view_products(request):
+    #select all the products from the database
+    products= Product.objects.all()
+    #render the template with products
+    return render(request, 'products.html', {'products':products})
+
+@login_required
+def delete_product(request,id):
+    #select the product you need to delete
+    product = Product.objects.get(id=id)
+    #finally delete the product
+    product.delete()
+    #Redirect back to products page with a success message
+    messages.success(request, 'Product deleted successfully')
+    return redirect('products')
+
+@login_required
+def update_products(request, id):
+    #select the product to be updated
+    product = Product.objects.get(id=id)
+    # Check if the form has any submitted records to receive them
+    if request.method == "POST":
+        updated_name = request.POST.get('jina')
+        updated_quantity = request.POST.get('kiasi')
+        updated_price = request.POST.get('bei')
+        # Update the selected product above with the received data
+        product.prod_name = updated_name
+        product.prod_quantity = updated_quantity
+        product.prod_price = updated_price
+
+        # Return the updated data back to the database
+        product.save()
+        # Redirect back to the products page with a success message
+        messages.success(request, 'product updated successfully')
+        return redirect('products')
+
+    return render(request, 'update-product.html',{'product': product})
+
+
+
+
+
+
+
+
+
+
